e7fb4163acb2eaa3ddfe0593ea466fa4d84ac929
Marshallhch <marshallhch@gmail.com>
2021-09-30 20:29:16 +0900

    listing product complete

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { createStoreHook } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers/index";
+
+const store = createStore(
+  reducers,
+  {},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
